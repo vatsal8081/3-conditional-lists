abaec007999d0e399ae43a6a2b101a2978d2c411
Vatsal Patel <vatsal.patel@esparkbizmail.com>
2021-09-01 19:04:19 +0530

    loop practic added

--- loops.js
+++ loops.js
@@ -3,8 +3,40 @@ const app = Vue.createApp({
     data(){
         return {
             fruits: ['apple', 'banana'],
-            user: {name: 'vatsal', age: 23}
+            user: {name: 'vatsal', age: 23},
+            counter: 0,
+            todo: [],
+            todoInput: ""
         };
+    },
+
+    computed: {
+
+        getNames(){
+            console.log('in computed');
+            return ['vatsal', 'sarvesh', 'shivam']
+        }
+
+    },
+
+
+    methods: {
+        getNamesMethod(){
+            console.log('in method');
+            return ['vatsal', 'sarvesh', 'shivam']
+        },
+
+        addTodo(){
+            if (!this.todoInput) {
+                return;
+            }
+            this.todo.push(this.todoInput)
+            this.todoInput = ""
+        },
+
+        removeTodo(i){
+            this.todo.splice(i,1);
+        }
     }
 });
 
